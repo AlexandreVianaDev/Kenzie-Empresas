676ff39a71e196de67f76043802c93095158ec4b
AlexandreVianaDev <alexandreviana94@hotmail.com>
2023-01-06 15:33:04 -0300

    HTML e CSS concluídos

--- src/scripts/dashboard.js
+++ src/scripts/dashboard.js
@@ -1,3 +1,27 @@
-const modal = document.querySelector(".modal__departament--control")
+const modalDepartament = document.querySelector(".modal__departament--control")
 
-modal.showModal()
+const modalCreateDepartament = document.querySelector(".modal__departament--create")
+
+const modalEditDepartament = document.querySelector(".modal__departament--edit")
+
+const modalEditUser = document.querySelector(".modal__user--edit")
+
+const modalWarningUserRemove = document.querySelector(".modal__warning--user-remove")
+
+const modalWarningDepartamentRemove = document.querySelector(".modal__warning--departament-remove")
+
+const modalEditProfile = document.querySelector(".modal__profile--edit")
+
+// modalDepartament.showModal()
+
+// modalCreateDepartament.showModal()
+
+// modalEditDepartament.showModal()
+
+// modalEditUser.showModal()
+
+// modalWarningUserRemove.showModal()
+
+// modalWarningDepartamentRemove.showModal()
+
+// modalEditProfile.showModal()
